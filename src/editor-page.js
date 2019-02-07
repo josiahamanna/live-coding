26f7ed831725bcb7f95f31608d2c8fbbeb9b6ed1
nissi_shalom <jnamanna@gmail.com>
2019-02-07 17:09:53 +0530

    implemented react-ace editor in front end

--- src/editor-page.js
+++ src/editor-page.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 import io from 'socket.io-client'
+import Editor from './editor'
+import { SERVER_URL } from './config/config'
 
 class EditorPage extends Component {
+
+    constructor(props) {
+        super(props)
+        this.handleChange = this.handleChange.bind(this)
+        this.socket = io(`${SERVER_URL}/${props.location.state.url}`)
+    }
+
+    handleChange(data) {
+        this.socket.emit('SEND_MESSAGE', data)
+    }
+
     render() {
         return (
             <div>
-                <h2> Editor page </h2>
+                <p>
+                    Share <span onClick={
+                        function (event) {
+                            navigator.clipboard.writeText(event.target.innerHTML)
+                            document.execCommand("copy")
+                        }
+                    } title="click to copy"><u><b>{this.props.location.state.url}</b></u></span>
+                </p>
+                <Editor handleChange={this.handleChange} socket={this.socket} />
             </div>
         )
     }
--- src/editor.js
+++ src/editor.js
@@ -1,12 +1,123 @@
 import React from 'react'
 import AceEditor from 'react-ace'
 
+import 'brace/mode/javascript'
+import 'brace/theme/monokai'
+import 'brace/ext/language_tools'
+
+const languages = [
+    'javascript',
+    'java',
+    'python',
+    'ruby'
+]
+
+const themes = [
+    'monokai',
+    'github',
+    'tomorrow',
+    'kuroir',
+    'twilight',
+    'xcode',
+    'textmate',
+    'solarized_dark',
+    'solarized_light',
+    'terminal',
+]
+
+languages.forEach(lang => {
+    require(`brace/mode/${lang}`)
+    require(`brace/snippets/${lang}`)
+})
+
+themes.forEach(theme => {
+    require(`brace/theme/${theme}`)
+})
+
 class Editor extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: '',
+            theme: 'monokai',
+            mode: 'javascript',
+        }
+        this.setTheme = this.setTheme.bind(this);
+        this.setMode = this.setMode.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.props.socket.on('RECEIVE_MESSAGE', (data) => {
+            this.setState({ value: data })
+        })
+        this.props.socket.on('RECEIVE_NEW_LANGUAGE', (data) => {
+            if (data)
+                this.setState({ mode: data })
+        })
+    }
+
+    onChange(newValue) {
+        this.setState({ value: newValue })
+        this.props.handleChange(newValue)
+    }
+
+    setTheme(e) {
+        this.setState({
+            theme: e.target.value,
+        });
+    }
+    setMode(e) {
+        this.setState({
+            mode: e.target.value,
+        });
+        this.props.socket.emit('CHANGED_LANGUAGE', e.target.value)
+    }
+
     render() {
         return (
-            <div>
-                <h2> editor </h2>
-            </div>
+            <div className="editorClass" >
+                <select name="mode" onChange={this.setMode} value={this.state.mode}>
+                    {languages.map(lang => (
+                        <option key={lang} value={lang}>
+                            {lang}
+                        </option>
+                    ))}
+                </select>
+                <br />
+                <select name="Theme" onChange={this.setTheme} value={this.state.theme}>
+                    {themes.map(theme => (
+                        <option key={theme} value={theme}>
+                            {theme}
+                        </option>
+                    ))}
+                </select>
+                <br />
+                <AceEditor
+                    name="editor1"
+                    mode={this.state.mode}
+                    theme={this.state.theme}
+                    height='400px'
+                    width='600px'
+                    value={this.state.value}
+                    fontSize={14}
+                    showGutter={true}
+                    wrapEnabled={true}
+                    highlightActiveLine={true}
+                    showPrintMargin={true}
+                    focus={true} // to focus on editor automatically when page loads
+                    readOnly={false} // to make read only instance. it will be helpful when we just want listeners to juse see the code.
+                    onLoad={this.onLoad}
+                    onChange={this.onChange}
+
+                    setOptions={{
+                        enableBasicAutocompletion: true,
+                        enableLiveAutocompletion: true,
+                        enableSnippets: false,
+                        showLineNumbers: true,
+                        tabSize: 4,
+                    }}
+                    editorProps={{ $blockScrolling: Infinity }}
+                />
+            </div >
         )
     }
 }
