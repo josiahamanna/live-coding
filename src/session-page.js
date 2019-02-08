c86416177304972edaf21510b454c47399c5ca19
nissi_shalom <jnamanna@gmail.com>
2019-02-08 16:45:48 +0530

    Prevent users from going back to editor page once session is destroyed

--- src/session-page.js
+++ src/session-page.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import { SERVER_URL } from './config/config'
 
-const SessionPage = (props) => {
+class SessionPage extends React.Component {
 
-    function handleSubmit(event) {
+    constructor() {
+        super()
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.createSession = this.createSession.bind(this)
+    }
+
+    componentDidMount() {
+        /* make sure that the user is not able to go forward again to the editor page once session is destroye */
+        window.history.pushState(null, document.title, document.location.href)
+        window.addEventListener('popstate', function (event) {
+            this.window.history.pushState(null, document.title, document.location.href)
+        })
+    }
+
+    handleSubmit(event) {
         event.preventDefault()
         const url = event.target[0].value
         if (url.length === 0) {
@@ -13,41 +27,43 @@ const SessionPage = (props) => {
                 .then(res => res.json())
                 .then(res => {
                     if (res.isSocketPresent) {
-                        props.history.push({
+                        this.props.history.push({
                             pathname: '/editor-page',
                             state: { url }
                         })
                     } else {
                         if (window.confirm('this URL does not exist. Do you want to create a new session?')) {
-                            createSession()
+                            this.createSession()
                         }
                     }
                 })
         }
     }
 
-    function createSession() {
+    createSession() {
         fetch(`${SERVER_URL}/getSessionURL`)
             .then(res => res.json())
             .then(res => {
                 const url = res.url
-                props.history.push({ pathname: '/editor-page', state: { url } })
+                this.props.history.push({ pathname: '/editor-page', state: { url } })
             })
             .catch(err => console.log(err))
     }
 
-    return (
-        <div>
-            <button onClick={createSession}> Create New Session </button>
-            <br /> <br />
-            <h3> OR </h3>
-            <br />
-            <form onSubmit={handleSubmit}>
-                <input type='text' placeholder='Type the URL to connect...' />
-                <input type='submit' value='connect' />
-            </form>
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <button onClick={this.createSession}> Create New Session </button>
+                <br /> <br />
+                <h3> OR </h3>
+                <br />
+                <form onSubmit={this.handleSubmit}>
+                    <input type='text' placeholder='Type the URL to connect...' />
+                    <input type='submit' value='connect' />
+                </form>
+            </div>
+        )
+    }
 }
 
 export default SessionPage
